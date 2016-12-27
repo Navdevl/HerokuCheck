d4b835825fa22f4851809a9cc2c1de35f9489eb2
Nav_devl <Naveendurai@users.noreply.github.com>
2016-12-27 19:33:28 +0530

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Win Surprise Gifts</title>
+    <link rel="icon" href="favicon.ico">
+	<link rel="stylesheet" href="assets/demo.css">
+	<link rel="stylesheet" href="assets/form-labels-on-top.css">
+
+</head>
+
+
+	<header>
+	    <img src="assets/favicon.jpg" alt="Gift-Box" width="150px">
+		<h1>2017's Chance to Win Surprise Gifts.</h1>
+    </header>
+    <div class="heading-caption">
+        <p>Check your luck</p>    
+    </div>
+    
+
+
+    <div class="main-content">
+
+        <!-- You only need this form and the form-labels-on-top.css -->
+
+        <form class="form-labels-on-top" method="post" action="#">
+
+            <div class="form-title-row">
+                <h1>Load and Smash it with your Details</h1>
+            </div>
+
+            <div class="form-row">
+                <label>
+                    <span>Name</span>
+                    <input type="text" name="name">
+                </label>
+            </div>
+
+            <div class="form-row">
+                <label>
+                    <span>Email</span>
+                    <input type="email" name="email">
+                </label>
+            </div>
+
+            <div class="form-row">
+                <label><span>Gender</span></label>
+                <div class="form-radio-buttons">
+
+                    <div>
+                        <label>
+                            <input type="radio" name="radio">
+                            <span>Male</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <input type="radio" name="radio">
+                            <span>Female</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <input type="radio" name="radio">
+                            <span>Other</span>
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            
+            <div class="form-row">
+                <label>
+                    <span>District</span>
+                    <select name="dropdown">
+                        <option>Option One</option>
+                        <option>Option Two</option>
+                        <option>Option Three</option>
+                        <option>Option Four</option>
+                    </select>
+                </label>
+            </div>
+
+
+           
+
+            <div class="form-row">
+                <button type="submit">Submit Form</button>
+            </div>
+
+        </form>
+
+    </div>
+
+</body>
+
+</html>
